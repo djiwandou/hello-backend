325cd9ec9539072af792c3d469655dc04485e5a2
djiwandou <djiwandou@schoters.com>
2019-07-26 00:18:22 +0700

    add URL validation check

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,11 +4,10 @@ import (
     "fmt"
     "html/template"
     "log"
-    "net/http"
+	"net/http"
+	"net/url"
 	"strings"
-	"hello-backend/test"
-	// "encoding/json"
-	// "time"
+	"hello-backend/test"	
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -34,8 +33,13 @@ func checkURL(w http.ResponseWriter, r *http.Request) {
         r.ParseForm()
 		// logic part of check URL
 		url_input := r.Form["url_input"][0]
-		fmt.Println("url_input:", url_input)    
-		constructCheckup(w, url_input)    		
+		fmt.Println("url_input:", url_input)   
+		if isValidUrl(url_input) {
+			constructCheckup(w, url_input)
+		} else {
+			fmt.Fprintf(w, "Please Input a valid URL other than: %v\n", url_input) 
+		}
+		    		
     }
 }
 
@@ -49,6 +53,11 @@ func constructCheckup(w http.ResponseWriter, url_input string) {
 	}
 	fmt.Fprintf(w, "URL input: %v\n", url_input) 
 	fmt.Fprintf(w, "\nResults: %v\n", result) 
+}
+
+func isValidUrl(str string) bool {
+    u, err := url.Parse(str)
+    return err == nil && u.Scheme != "" && u.Host != ""
 }
 
 func main() {
